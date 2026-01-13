0915a4dee610b42815c025f5806870bd653e9268
ross <r0ssx@MacBookAir.fritz.box>
2026-01-13 18:11:28 +0100

    Working on request handling mechanism

--- src/main/java/Server/RequestHandler/AdminModifyProductHandler.java
+++ src/main/java/Server/RequestHandler/AdminModifyProductHandler.java
@@ -1,4 +1,25 @@
 package Server.RequestHandler;
 
-public class AdminModifyProductHandler {
+import Shared.Requests.Request;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.sql.SQLException;
+
+public class AdminModifyProductHandler extends AbstractRequestHandler{
+
+    /**
+     * Costruisce un handler associato a uno specifico tipo di richiesta.
+     * @param request il tipo di {@link Request} che questo handler può gestire
+     */
+    public AdminModifyProductHandler(Request request) {
+        super(request);
+    }
+
+    @Override
+    public void handleRequest(Request request, Socket socket) throws SQLException, IOException {
+        System.out.println("chiamata handleRequest di AdminModifyProductHandler");
+
+
+    }
 }
