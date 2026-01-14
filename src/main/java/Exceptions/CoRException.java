ba554b3be66e1ff0f5f57cfeb3ffb26b6c0a27f7
ross <r0ssx@MacBookAir.fritz.box>
2026-01-14 13:33:43 +0100

    Added comments

--- src/main/java/Exceptions/CoRException.java
+++ src/main/java/Exceptions/CoRException.java
@@ -1,6 +1,17 @@
 package Exceptions;
 
+/**
+ * Eccezione personalizzata utilizzata nel contesto del pattern
+ * Chain of Responsibility (CoR).
+ * Estende {@link RuntimeException} e viene lanciata per indicare
+ * errori specifici durante l'esecuzione di una catena di responsabilità.
+ */
 public class CoRException extends RuntimeException {
+
+    /**
+     * Costruisce una nuova eccezione CoR con un messaggio specifico.
+     * @param message messaggio descrittivo dell'eccezione
+     */
     public CoRException(String message) {
         super(message);
     }
